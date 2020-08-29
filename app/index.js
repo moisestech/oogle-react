972b2a13a58fea1934915f3ca112c23988b9c8a1
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-29 06:41:12 -0400

    intiate css, html, and index root rendering with ReactDOM

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Component
+// State
+// Lifecycle
+// UI
+
+export default function App () {
+
+  return (
+    <div>Welcome to Oogle Search</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
