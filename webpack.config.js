7be1f9efc95eba5bb4ee887167d6aad8ab355078
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-19 14:46:13 -0500

    new dir to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
@@ -13,10 +14,15 @@ module.exports = {
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
+      template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public/_redirects' }
+      ]
     })
   ]
 }
