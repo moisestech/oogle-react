349149c54f4b0a4380e3055300c099d1de9b6817
Moises Sanabria <sesmoi.data@gmail.com>
2021-01-02 17:43:23 -0500

    func comp search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import SearchIcon from '@material-ui/icons/Search'
-import MicIcon from '@material-ui/icons/Mic'
-import { Button } from '@material-ui/core'
+import React from "react";
+import PropTypes from "prop-types";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
-export default function Search () {
+export default function Search() {
   return (
-    <div className='search'>
-      <div className='search-input'>
-        <SearchIcon className='search-input-icon' />
+    <div className="search">
+      <div className="search-input">
+        <SearchIcon className="search-input-icon" />
         <input />
         <MicIcon />
       </div>
-      <div className='search-buttons'>
+      <div className="search-buttons">
         <Button>Google Search</Button>
-        <Button variant='outline'>I'm Feeling Lucky</Button>
+        <Button variant="outline">I'm Feeling Lucky</Button>
       </div>
     </div>
-  )
+  );
 }
